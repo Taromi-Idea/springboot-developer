cbf0d4999917370844df4c98a25e7ebf22671e98
Taromi <lovetip3@gmail.com>
2023-05-24 23:18:03 +0900

    나머지 api 모두 구성 하고 테스트 작업 완료

--- src/main/java/com/taromi/springbootdeveloper/controller/BlogApiController.java
+++ src/main/java/com/taromi/springbootdeveloper/controller/BlogApiController.java
@@ -2,13 +2,15 @@ package com.taromi.springbootdeveloper.controller;
 
 import com.taromi.springbootdeveloper.domain.Article;
 import com.taromi.springbootdeveloper.dto.AddArticleRequest;
+import com.taromi.springbootdeveloper.dto.ArticleResponse;
+import com.taromi.springbootdeveloper.dto.UpdateArticleRequest;
 import com.taromi.springbootdeveloper.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController // http response body에 객체 데이터를 json 형식으로 반환하는 컨트롤러
@@ -22,5 +24,41 @@ public class BlogApiController {
 
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(savedArticle);
+    }
+
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles() {
+        List<ArticleResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
+
+        return ResponseEntity.ok()
+                .body(articles);
+    }
+
+    @GetMapping("/api/articles/{id}")
+    // URL 경로에서 값 추출
+    public ResponseEntity<ArticleResponse> findArticle(@PathVariable("id") long id) {
+        Article article = blogService.findById(id);
+
+        return ResponseEntity.ok()
+                .body(new ArticleResponse(article));
+    }
+
+    @DeleteMapping("/api/articles/{id}")
+    public ResponseEntity<Void> deleteArticle(@PathVariable long id) {
+        blogService.delete(id);
+
+        return ResponseEntity.ok()
+                .build();
+    }
+
+    @PutMapping("/api/articles/{id}")
+    public ResponseEntity<Article> updateArticle(@PathVariable long id, @RequestBody UpdateArticleRequest request) {
+        Article updateArticle = blogService.update(id, request);
+
+        return ResponseEntity.ok()
+                .body(updateArticle);
     }
 }
--- src/main/java/com/taromi/springbootdeveloper/service/BlogService.java
+++ src/main/java/com/taromi/springbootdeveloper/service/BlogService.java
@@ -2,9 +2,13 @@ package com.taromi.springbootdeveloper.service;
 
 import com.taromi.springbootdeveloper.domain.Article;
 import com.taromi.springbootdeveloper.dto.AddArticleRequest;
+import com.taromi.springbootdeveloper.dto.UpdateArticleRequest;
 import com.taromi.springbootdeveloper.repository.BlogRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @RequiredArgsConstructor // final이 붙거나 @NotNull이 붙은 필드 생성자 추가
 @Service // 빈으로 등록
@@ -14,5 +18,28 @@ public class BlogService {
     // 블로그 글 추가 메서드
     public Article save(AddArticleRequest request) {
         return blogRepository.save(request.toEntity());
+    }
+
+    public List<Article> findAll() {
+        return blogRepository.findAll();
+    }
+
+    public Article findById(long id) {
+        return blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found: " + id));
+    }
+
+    public void delete(long id) {
+        blogRepository.deleteById(id);
+    }
+
+    @Transactional // 트랜잭션 메서드
+    public Article update(long id, UpdateArticleRequest request) {
+        Article article = blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found: " + id));
+
+        article.update(request.getTitle(), request.getContent());
+
+        return article;
     }
 }
