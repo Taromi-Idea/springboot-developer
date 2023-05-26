24b8ff9a967387b0052c7db96f74cbad444a1fe4
Taromi <lovetip3@gmail.com>
2023-05-26 22:17:42 +0900

    글 상세보기 작성

--- src/main/java/com/taromi/springbootdeveloper/controller/BlogViewController.java
+++ src/main/java/com/taromi/springbootdeveloper/controller/BlogViewController.java
@@ -1,11 +1,14 @@
 package com.taromi.springbootdeveloper.controller;
 
+import com.taromi.springbootdeveloper.domain.Article;
 import com.taromi.springbootdeveloper.dto.ArticleListViewResponse;
+import com.taromi.springbootdeveloper.dto.ArticleViewResponse;
 import com.taromi.springbootdeveloper.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -22,5 +25,13 @@ public class BlogViewController {
         model.addAttribute("articles", articles); // 블로그 글 리스트 저장
 
         return "articleList"; // articleList.html 라는 뷰 조회
+    }
+
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable Long id, Model model) {
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleViewResponse(article));
+
+        return "article";
     }
 }
