4a0abefef38e7fa67e7f8dd287cd6ab0014fcd05
Taromi <lovetip3@gmail.com>
2023-05-26 20:50:03 +0900

    블로그 글 목록 제작

---
+++ src/main/java/com/taromi/springbootdeveloper/controller/BlogViewController.java
@@ -0,0 +1,26 @@
+package com.taromi.springbootdeveloper.controller;
+
+import com.taromi.springbootdeveloper.dto.ArticleListViewResponse;
+import com.taromi.springbootdeveloper.service.BlogService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class BlogViewController {
+    private final BlogService blogService;
+
+    @GetMapping("/articles")
+    public String getArticles(Model model) {
+        List<ArticleListViewResponse> articles = blogService.findAll().stream()
+                .map(ArticleListViewResponse::new)
+                .toList();
+        model.addAttribute("articles", articles); // 블로그 글 리스트 저장
+
+        return "articleList"; // articleList.html 라는 뷰 조회
+    }
+}
